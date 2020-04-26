e07c4e7f6944b466a65ac3a94600e30cf5f57e76
Jerry <jerry@exntu.com>
2020-04-26 23:50:26 +0900

    [공부] JPA - JPQL

---
+++ jpql/src/main/java/me/jerry/jpql/MemberDTO.java
@@ -0,0 +1,14 @@
+package me.jerry.jpql;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class MemberDTO {
+
+    private String username;
+    private int age;
+}
